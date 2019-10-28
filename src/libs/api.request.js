d8bc1e5ca51bfa9ff820c5a7cc6bf287f18085b1
jyhs <jyhs_123@163.com>
2019-10-28 18:05:26 +0800

    update

--- src/libs/api.request.js
+++ src/libs/api.request.js
@@ -1,6 +1,6 @@
 import HttpRequest from '@/libs/axios'
 // import config from '@/config'
 // const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro
-const baseUrl = 'https://hsapi.100eduonline.com/'
+const baseUrl = 'https://gzhgl.sufe.edu.cn/'
 const axios = new HttpRequest(baseUrl)
 export default axios
